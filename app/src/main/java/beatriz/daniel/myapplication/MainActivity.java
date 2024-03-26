bdf35d33f64e9f6ae08554d29a5f1047b55e8450
Beatriz Alvarenga Rocha <20212TIOT0359@cefetes.br>
2024-03-26 14:27:54 -0300

    Initial commit

---
+++ app/src/main/java/beatriz/daniel/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package beatriz.daniel.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // cria uma activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnEnviar = findViewById(R. id. btn_enviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etDigiteAqui = findViewById(R. id. et_digite_aqui);
+                String textoDigitado = etDigiteAqui.getText().toString();
+                Intent i = new Intent(MainActivity.this, NextActivity.class);
+                i.putExtra("texto", textoDigitado);
+                startActivity(i);
+
+            }
+        });
+    }
+}
