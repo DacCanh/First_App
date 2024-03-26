c98b343228fbffe869f64f8f2e922bb1601336e8
Daneatriz <danielcanhamaque@gmail.com>
2024-03-26 14:39:41 -0300

    Comentário do professor

--- app/src/main/java/beatriz/daniel/myapplication/MainActivity.java
+++ app/src/main/java/beatriz/daniel/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText etDigiteAqui = findViewById(R. id. et_digite_aqui);
+                EditText etDigiteAqui = findViewById(R. id. et_digite_aqui);//obtém a caixa de texto
                 String textoDigitado = etDigiteAqui.getText().toString();
                 Intent i = new Intent(MainActivity.this, NextActivity.class);
                 i.putExtra("texto", textoDigitado);
